e0829a5edbbf7a5ac9e5711c2c960db2d52521b5
xengom <xengom@gmail.com>
2021-05-26 23:50:13 +0900

    VO Mapper Pom(Dependency) mybatic-config root-context

---
+++ music/src/main/java/com/tj/music/domain/musicVO.java
@@ -0,0 +1,41 @@
+package com.tj.music.domain;
+
+import java.util.Date;
+
+public class musicVO {
+	private int bno;
+	private String title;
+	private String singer;
+	private Date regdate;
+	private int viewcnt;
+	public int getBno() {
+		return bno;
+	}
+	public void setBno(int bno) {
+		this.bno = bno;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getSinger() {
+		return singer;
+	}
+	public void setSinger(String singer) {
+		this.singer = singer;
+	}
+	public Date getRegdate() {
+		return regdate;
+	}
+	public void setRegdate(Date regdate) {
+		this.regdate = regdate;
+	}
+	public int getViewcnt() {
+		return viewcnt;
+	}
+	public void setViewcnt(int viewcnt) {
+		this.viewcnt = viewcnt;
+	}
+}
