00afe48c6cf43934114afab87e63738aacf5c641
xengom <xengom@gmail.com>
2021-05-28 00:53:25 +0900

    Backup 5/28 12:53

---
+++ music/src/main/java/com/tj/music/youtube/Search.java
@@ -0,0 +1,33 @@
+package com.tj.music.youtube;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+
+
+public class Search {
+	public String search(String search) throws IOException {
+		
+		String apiurl = "https://www.googleapis.com/youtube/v3/search";
+		apiurl += "?key=AIzaSyA0E7WSRk5FVVvIClEkV82rf19HqY54IMQ";
+		apiurl += "&part=snippet&type=video&maxResults=1&videoEmbeddable=true";
+		apiurl += "&q="+URLEncoder.encode(search,"UTF-8");
+		
+		URL url = new URL(apiurl);
+		HttpURLConnection con = (HttpURLConnection) url.openConnection();
+		con.setRequestMethod("GET");
+		
+		BufferedReader br = new BufferedReader(new java.io.InputStreamReader(con.getInputStream(),"UTF-8"));
+		System.out.println(br);
+		String inputLine;
+		StringBuffer response = new StringBuffer();
+		
+		while((inputLine = br.readLine()) != null) {
+			response.append(inputLine);
+		}
+		br.close();
+		return response.toString();
+	}
+}
